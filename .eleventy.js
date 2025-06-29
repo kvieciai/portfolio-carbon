5cc5218a099c0bb813b66016997233ebb338966e
kvieciai <aleksnadr.anciutin@gmaill.com>
2025-06-29 16:34:19 +0200

    added robots.txt

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,7 @@ module.exports = (config) => {
     // Set directories to pass through to the dist folder
     config.addPassthroughCopy('./src/images/');
 	config.addPassthroughCopy("./src/fonts/");
+	config.addPassthroughCopy("./src/robots.txt");
 
 	// Minify inline CSS using cssmin filter
 	config.addFilter("cssmin", function (code) {
